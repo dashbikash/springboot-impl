6ef774219d605e0776f955d2083b949c5a013d9f
Bikash <bikash@bikash-pc>
2024-07-23 00:55:56 +0530

    Update IntegrationConfig.java and SpringIntegrationApplication.java

--- spring_integration/src/main/java/dashbikash/spring_integration/IntegrationConfig.java
+++ spring_integration/src/main/java/dashbikash/spring_integration/IntegrationConfig.java
@@ -1,21 +1,25 @@
 package dashbikash.spring_integration;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.filters.SimplePatternFileListFilter;
-import org.springframework.messaging.MessageChannel;
+import org.springframework.integration.graph.IntegrationGraphServer;
+import org.springframework.integration.http.config.EnableIntegrationGraphController;
 
 @Configuration
 @EnableAutoConfiguration
+@EnableIntegrationGraphController(allowedOrigins = {"*"})
 public class IntegrationConfig {
 //	@Bean
 //	public MessageChannel fileChannel() {
@@ -38,6 +42,23 @@ public class IntegrationConfig {
 		writingMessageHandler.setAutoCreateDirectory(true);
 		writingMessageHandler.setExpectReply(false);
 		return writingMessageHandler;
+	}
+	
+	@Bean
+	public IntegrationGraphServer integrationGraphServer()
+	{
+		IntegrationGraphServer server=new IntegrationGraphServer();
+		server.setAdditionalPropertiesCallback(namedComponent -> {
+            Map<String, Object> properties = null;
+            if (namedComponent instanceof SmartLifecycle) {
+                SmartLifecycle smartLifecycle = (SmartLifecycle) namedComponent;
+                properties = new HashMap<>();
+                properties.put("auto-startup", smartLifecycle.isAutoStartup());
+                properties.put("running", smartLifecycle.isRunning());
+            }
+            return properties;
+        });
+		return server;
 	}
 	
 }
