b78592c08ff4257283092c009417f71e111bccdb
Bikash <bikashprakashdash@gmail.com>
2024-12-04 03:20:49 +0530

    Zookeeper impl

--- spring_integration/src/main/java/dashbikash/spring_integration/IntegrationConfig.java
+++ spring_integration/src/main/java/dashbikash/spring_integration/IntegrationConfig.java
@@ -1,9 +1,6 @@
 package dashbikash.spring_integration;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -13,22 +10,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.InboundChannelAdapter;
 import org.springframework.integration.annotation.Poller;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
 import org.springframework.integration.file.filters.CompositeFileListFilter;
 import org.springframework.integration.file.filters.SimplePatternFileListFilter;
 import org.springframework.integration.graph.IntegrationGraphServer;
 import org.springframework.integration.http.config.EnableIntegrationGraphController;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableIntegrationGraphController(allowedOrigins = {"*"})
 public class IntegrationConfig {
-//	@Bean
-//	public MessageChannel fileChannel() {
-//		return new DirectChannel();
-//	}
-//	
+	
+	@Bean
+    public MessageChannel fileInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel fileOutputChannel() {
+        return new DirectChannel();
+    }
+
+	
 	@Value("${flow.source_dir}")
 	private String SOURCE_DIR;
 	@Value("${flow.destination_dir}")
@@ -36,9 +47,13 @@ public class IntegrationConfig {
 	
 	@Autowired
 	private FileNotExistFilter existFilter;
+
+	@Autowired
+    private InterProcessMutex lock;
+	
 	
 	@Bean
-	@InboundChannelAdapter(value = "fileChannel",poller = @Poller(fixedDelay = "1000"))
+	@InboundChannelAdapter(value = "fileInputChannel",poller = @Poller(fixedDelay = "1000"))
 	public FileReadingMessageSource fileReadingMessageSource() {
 		FileReadingMessageSource readingMessageSource=new FileReadingMessageSource();
 		readingMessageSource.setDirectory(new File(SOURCE_DIR));
@@ -50,12 +65,41 @@ public class IntegrationConfig {
 		readingMessageSource.setFilter(compositeFileListFilter);
 		return readingMessageSource;
 	}
+	
+	
 	@Bean
-	@ServiceActivator(inputChannel =  "fileChannel")
+    @ServiceActivator(inputChannel = "fileInputChannel")
+    public MessageHandler fileProcessor() {
+        return message -> {
+            try {
+                lock.acquire();
+                // Process the file
+                File inputFile = (File) message.getPayload();
+                // Add your file processing logic here
+                Thread.sleep(10000);
+                // Pass the message to the output channel for writing
+                fileOutputChannel().send(message);
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    lock.release();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+
+	@Bean
+	@ServiceActivator(inputChannel =  "fileOutputChannel")
 	public FileWritingMessageHandler fileWritingMessageHandler() {
 		FileWritingMessageHandler writingMessageHandler=new FileWritingMessageHandler(new File(DESTINATION_DIR));
 		writingMessageHandler.setAutoCreateDirectory(true);
 		writingMessageHandler.setExpectReply(false);
+		
 		return writingMessageHandler;
 	}
 	
@@ -75,5 +119,7 @@ public class IntegrationConfig {
         });
 		return server;
 	}
+
+	
 	
 }
--- spring_integration/src/main/java/dashbikash/spring_integration/ZookeeperConfig.java
+++ spring_integration/src/main/java/dashbikash/spring_integration/ZookeeperConfig.java
@@ -2,30 +2,24 @@ package dashbikash.spring_integration;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.config.EnableIntegration;
-import org.springframework.integration.zookeeper.config.LeaderInitiatorFactoryBean;
-import org.springframework.integration.zookeeper.metadata.ZookeeperMetadataStore;
 
 @Configuration
-@EnableIntegration
 public class ZookeeperConfig {
 
     @Bean
     public CuratorFramework curatorFramework() {
-        return CuratorFrameworkFactory.newClient("localhost:2181", new ExponentialBackoffRetry(1000, 3));
+        CuratorFramework client = CuratorFrameworkFactory.newClient(
+                "localhost:2181", new ExponentialBackoffRetry(1000, 3));
+        client.start();
+        return client;
     }
 
     @Bean
-    public ZookeeperMetadataStore zookeeperMetadataStore(CuratorFramework curatorFramework) {
-        return new ZookeeperMetadataStore(curatorFramework);
+    public InterProcessMutex lock(CuratorFramework client) {
+        return new InterProcessMutex(client, "/distributed-lock");
     }
-
-    @Bean
-    public LeaderInitiatorFactoryBean leaderInitiatorFactoryBean(CuratorFramework client) {
-        return new LeaderInitiatorFactoryBean().setClient(client).setPath("/leader");
-    }
-
 }
